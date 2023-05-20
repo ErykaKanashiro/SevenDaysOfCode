cd30415679b0a8cbcd3bad5e72bdbc3e47f7f2b3
Eryka <erykakanshiro@gmail.com>
2023-05-20 20:04:39 -0300

    ERK-01, get do top 250 filmes do IMDB. 1/7. #7DaysOfCode

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,39 @@
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws URISyntaxException {
+
+        System.out.println("Requisição TOP 250 Filmes do IMDB");
+
+        String apiKey = "XXXXXXXX";
+
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpRequest httpRequest = HttpRequest.newBuilder()
+                .uri(new URI("https://imdb-api.com/en/API/Top250Movies/" + apiKey))
+                .GET()
+                .build();
+
+        try {
+
+            HttpResponse<String> response = client.send
+                    (httpRequest, HttpResponse.BodyHandlers.ofString());
+
+            int statusCode = response.statusCode();
+            String responseBody = response.body();
+
+            System.out.println("Status: " + statusCode);
+            System.out.println("Body: " + responseBody);
+
+        } catch (Exception e) {
+
+            System.out.println("Erro ao fazer a requisição: " + e.getMessage());
+
+        }
+
     }
 }
